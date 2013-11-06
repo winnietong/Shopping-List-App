370a6fee2425da31fbed7b20936db04147c1ee84
Winnie Tong <tong.winnie@gmail.com>
2013-11-05 17:05:58 -0800

    Add Item functions
    Add Item function for green box click and enter key submit.

--- application.js
+++ application.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
 	$( ".green-box" ).click(function(){
-		itemToAdd = $("#item").val();
-		var createItem = '<div class="add-item"><div class="red-box"><input type="checkbox" class="checkbox" value""></div><input type="text" class="item-field" value="' + itemToAdd + '"></div>'
-		$("#shopping-list").append(createItem);
+		addItem();
 	});
+
+	$("#item").keypress(function(e){
+		if(e.which == 13){ //if Enter key is pressed
+			addItem();
+			e.preventDefault();
+		}
+	});
+
+	function addItem(){
+		itemToAdd = $("#item").val();
+		var createItem = '<div class="add-item"><div class="red-box"><input type="checkbox" class="checkbox" value""></div><input type="text" class="item-field" value="' + itemToAdd + '"></div>';
+		$("#shopping-list").append(createItem);
+		$("#item").attr("placeholder", "Add an item").val("");
+	}
 
 	$( "#clear" ).click(function(){
 		$("input:checked").closest(".add-item").remove();
